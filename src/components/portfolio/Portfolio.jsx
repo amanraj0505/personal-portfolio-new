a7fb213ca3237d85bc60defe5008b46e03f95239
Raj <aman.raj@unisys.com>
2022-11-14 18:00:17 +0530

    development

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
-
+import Menu from "./Menu";
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const [items, setItems] = useState(Menu);
+  return (
+    <section className="work container section" id="work">
+      <h2 className="section__title">React Works</h2>
+
+      <div className="work__filters">
+        <span className="work__item">Everything</span>
+        <span className="work__item">Creative</span>
+        <span className="work__item">Design</span>
+      </div>
+
+      <div className="work__contianer grid">
+        {items.map((elem) => {
+          const { id, image, title, category } = elem;
+          return (
+            <div className="work__card" key={id}>
+              <div className="work__thumbnail">
+                <img src={image} alt="" className="work__img" />
+                <div className="work__mask"></div>
+
+                <span className="work__category">{category}</span>
+                <h3 className="work__title">{title}</h3>
+                <a href="#" className="work__button">
+                  <i className="icon-link work__button-icon"></i>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 import "./resume.css";
-
+import Data from "./Data";
+import Card from "./Card";
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <section className="resume container section" id="resume">
+      <h2 className="section__title">Experience</h2>
+      <div className="resume__container grid">
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            let component = "";
+            if (val.category === "education") {
+              component = (
+                <Card
+                  key={id}
+                  icon={val.icon}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              );
+            }
+            return component;
+          })}
+        </div>
+        <div className="timeline grid">
+          {Data.map((val, index) => {
+            let component = "";
+            if (val.category === "experience") {
+              component = (
+                <Card
+                  key={index}
+                  icon={val.icon}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              );
+            }
+            return component;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Resume;
