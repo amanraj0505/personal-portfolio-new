0633a0a97ef752095847918652aa4cdb1ed6e02c
Aman Raj <amanraj6437@gmail.com>
2024-10-15 01:57:05 +0530

    form validation

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,41 +1,73 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./contact.css";
 import emailjs from "@emailjs/browser";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const Contact = () => {
   const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [bodyError, setBodyError] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [subjectError, setSubjectError] = useState("");
+  const [body, setBody] = useState("");
   const sendEmail = (e) => {
-    const id = toast.loading("🤔 Sending message...");
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_jedb8ev",
-        "template_ttrhovy",
-        form.current,
-        "SxEUwQVRbAkf65faW"
-      )
-      .then(
-        (result) => {
-          toast.update(id, {
-            render: "😍 Message Sent!!",
-            type: "success",
-            isLoading: false,
-            autoClose: 3000,
-          });
-          form.current.reset();
-        },
-        (error) => {
-          console.log(error.text);
-          toast.update(id, {
-            render: "🥴 Something Went Wrong!!",
-            type: "error",
-            isLoading: false,
-            autoClose: 3000,
-          });
-          form.current.reset();
-        }
-      );
+    if (!(name && subject && email && body)) {
+      e.preventDefault();
+      setEmailError(!email);
+      setNameError(!name);
+      setSubjectError(!subject);
+      setBodyError(!body);
+      toast.error("some form fields are missing...");
+    } else {
+      const id = toast.loading("🤔 Sending message...");
+      e.preventDefault();
+      emailjs
+        .sendForm(
+          "service_jedb8ev",
+          "template_ttrhovy",
+          form.current,
+          "SxEUwQVRbAkf65faW"
+        )
+        .then(
+          () => {
+            toast.update(id, {
+              render: "😍 Message Sent!!",
+              type: "success",
+              isLoading: false,
+              autoClose: 3000,
+            });
+            form.current.reset();
+          },
+          () => {
+            toast.update(id, {
+              render: "🥴 Something Went Wrong!!",
+              type: "error",
+              isLoading: false,
+              autoClose: 3000,
+            });
+            form.current.reset();
+          }
+        );
+    }
+  };
+  const onNameChange = (e) => {
+    setName(e.target.value);
+    setNameError(!e.target.value);
+  };
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+    setEmailError(!e.target.value);
+  };
+  const onSubjectChange = (e) => {
+    setSubject(e.target.value);
+    setSubjectError(!e.target.value);
+  };
+  const onBodyChange = (e) => {
+    setBody(e.target.value);
+    setBodyError(!e.target.value);
   };
   return (
     <section className="contact container section" id="contact">
@@ -57,6 +89,12 @@ const Contact = () => {
                 type={"text"}
                 placeholder="Type your name"
                 name="user_name"
+                style={{
+                  ...(nameError
+                    ? { border: "1px solid hsl(353, 100%, 65%)" }
+                    : {}),
+                }}
+                onChange={onNameChange}
               />
             </div>
 
@@ -66,6 +104,12 @@ const Contact = () => {
                 type={"email"}
                 placeholder="Type your email"
                 name="user_email"
+                style={{
+                  ...(emailError
+                    ? { border: "1px solid hsl(353, 100%, 65%)" }
+                    : {}),
+                }}
+                onChange={onEmailChange}
               />
             </div>
           </div>
@@ -74,7 +118,13 @@ const Contact = () => {
               className="contact__form-input"
               type={"text"}
               name="user_subject"
+              style={{
+                ...(subjectError
+                  ? { border: "1px solid hsl(353, 100%, 65%)" }
+                  : {}),
+              }}
               placeholder="Type your subject"
+              onChange={onSubjectChange}
             />
           </div>
           <div className="contact__form-div contact__form-area">
@@ -83,8 +133,14 @@ const Contact = () => {
               id=""
               cols={"30"}
               row={"10"}
+              style={{
+                ...(bodyError
+                  ? { border: "1px solid hsl(353, 100%, 65%)" }
+                  : {}),
+              }}
               placeholder="Type your message..."
               className="contact__form-input"
+              onChange={onBodyChange}
             ></textarea>
           </div>
           <button className="btn">Send Message</button>
